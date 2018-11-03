88b8bd503c0239998705989e3d2d76759b1d9014
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2018-11-03 19:15:45 +0200

    - Add the bat file that has to be run for the first time to download the dataset.

---
+++ downloadDataSet.bat
@@ -0,0 +1,6 @@
+rem Download the dataset from the internet. This file is a zip file that 
+rem contain the 3 files [train.p, valid.p, test.p]
+wget https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/traffic-signs-data.zip --no-check-certificate
+
+unzip traffic-signs-data.zip
+
